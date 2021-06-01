d157555dbeb42ce9bcbf8124ea838ddf528e33e7
Craig Wilkinson <WILKINSONC2@GOOGLEMAIL.COM>
2021-06-01 13:22:58 +0100

    Rectified build issue

--- sql_dict.py
+++ sql_dict.py
@@ -130,8 +130,7 @@ try:
              "   \"http://www.w3.org/TR/html4/strict.dtd\">" \
              "<HTML>" \
              "<HEAD>" \
-             #"<LINK href=\"" + stylesheet + "\" rel=\"stylesheet\" type=\"text/css\">" \
-             "<style>" + + "</style>"
+             "<LINK href=\"" + stylesheet + "\" rel=\"stylesheet\" type=\"text/css\">" \
              "</HEAD>" \
              "<BODY> "
     htmlin = htmlin + open("./temp/dd_output.html", "r", encoding="utf-8").read()
